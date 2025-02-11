b8836452f0b251c10b8708d98cde4cb245f570f8
luan guerreiro <loguerreiro1637@gmail.com>
2025-02-11 12:51:56 -0400

    Create documentação da explicação de como executar o arquivo .sh

---
+++ documentação a explicação de como executar o arquivo .sh
@@ -0,0 +1,11 @@
+## Como executar o arquivo .sh
+
+1. Abra o terminal.
+
+2. Navegue até o diretório onde o arquivo `.sh` está localizado:
+   ```bash
+   cd /caminho/para/o/diretorio 
+   
+3. chmod +x executar_script.sh
+
+4. ./executar_script.sh
